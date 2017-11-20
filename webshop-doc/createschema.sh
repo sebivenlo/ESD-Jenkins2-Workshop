bbc2d993e58af16df3a905f021df74e28d546286
Nils <n.nieuwenhuis@student.fontys.nl>
2017-11-20 19:17:17 +0100

    removed unused files

--- webshop-doc/createschema.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-db=webshop
-echo $#
-rm -fr ${db}-schema
-if [ $# -ge 1 ]; then
-    db=$1
-fi
-java -jar /home/hom/Downloads/schemaSpy_5.0.0.jar -dp /usr/share/java/postgresql-jdbc4.jar -db ${db} -s public -u exam -p exam -t pgsql -host localhost -o ${db}-schema
