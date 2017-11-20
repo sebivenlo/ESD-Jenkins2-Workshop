a60b90c51e7faf55c1637d854df40619f4e55095
Nils <n.nieuwenhuis@student.fontys.nl>
2017-11-20 19:12:51 +0100

    remove webshop

--- webshop/model/dbscripts/createdb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-dropdb --if-exists webshop
-createdb -O exam webshop
-cat schema.sql | psql -X webshop
--- webshop/model/src/main/java/webshop/business/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * This package contains the business logic classes of the application. None of
- * the instances of the classes in this package are to be persisted to a
- * database. In particular, the ShoppingCart and its implementation and the
- * WebshopFacade have session scope at most and need not be stored.
- *
- * This package is a derivative of a previous practicum exercise for sen1.
- */
-package webshop.business;
